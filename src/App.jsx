1fdeed1d958ef134cd4efa83db988a3edecfc7ef
Jamie Bryson <jamieab98@gmail.com>
2026-05-17 11:52:42 -0400

    Made the SearchForm component and added it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import SearchForm from "./SearchForm"
 function App(){
 
   return(
     <div>
       <h1>App Component</h1>
+      <SearchForm/>
     </div>
   )
 }
---
+++ src/SearchForm.jsx
@@ -0,0 +1,10 @@
+function SearchForm(){
+
+    return(
+        <div>
+            <h3>Search Form</h3>
+        </div>
+    )
+}
+
+export default SearchForm
