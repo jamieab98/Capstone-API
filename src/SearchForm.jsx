2bbcd9df27d7365b8a1514db598ef3fb0995a52c
Jamie Bryson <jamieab98@gmail.com>
2026-05-17 12:11:45 -0400

    added the states for the region and country. Also added the attributes to link the states with the form fields

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react"
+
 function SearchForm(){
+    const [country, setCountry] = useState("")
+    const [region, setRegion] = useState("")
+
+    function handleSearch(e){
+        e.preventDefault()
+        console.log(`Looking for courses in ${country} and ${region}`)
+    }
 
     return(
         <div>
             <h3>Course Search</h3>
-            <form>
-                <label>Country Code:</label>
-                <input type="text"></input>
-                <label>Region Code:</label>
-                <input type="text"></input>
+            <form onSubmit={handleSearch}>
+                <label htmlFor="country">Country Code:</label>
+                <input type="text" id="country" onChange={(e)=>setCountry(e.target.value)} autoComplete="false"></input>
+                <label htmlFor="region">Region Code:</label>
+                <input type="text" id="region" onChange={(e)=>setRegion(e.target.value)} autoComplete="false"></input>
                 <button type="submit">Search</button>
             </form>
         </div>
