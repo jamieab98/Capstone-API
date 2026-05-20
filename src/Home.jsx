25d0b607fa365225e9930079386fa1d43246adba
Jamie Bryson <jamieab98@gmail.com>
2026-05-20 18:37:15 -0400

    Put some information in Home.jsx. Still needs to be styled

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,9 @@ function Home(){
 
     return(
         <>
-            <h1>Home Component</h1>
+            <h2>Welcome to U-Discovery</h2>
+            <div>This is an application that lets you find discgolf courses near you or wherever you please! All you need to know is the abbreviation to the country you're searching through and the region!</div>
+            <div>Click the find course tab at the top to start looking for courses!</div>
         </>
     )
 }
