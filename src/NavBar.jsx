bec55e7a16773a890d70120930b9aed3961b6e57
Jamie Bryson <jamieab98@gmail.com>
2026-05-22 17:19:43 -0400

    Styled the navigation bar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom"
+import styles from "./styling/NavBar.module.css"
 
 function NavBar(){
 
     return(
-        <>
-            <Link to="/">Home</Link>
-            <Link to="SearchDisplay">Find Courses</Link>
-            <Link to="About">About</Link>
-        </>
+        <div className={styles.navigation}>
+            <Link className={styles.link} to="/">Home</Link>
+            <Link className={styles.link} to="SearchDisplay">Find Courses</Link>
+            <Link className={styles.link} to="About">About</Link>
+        </div>
     )
 }
 
