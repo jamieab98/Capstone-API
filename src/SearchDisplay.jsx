e5e6ae4b3ff159916a26166b33ec22a0db51362c
Jamie Bryson <jamieab98@gmail.com>
2026-05-22 17:30:58 -0400

    Did some styling on the SearchDisplay component

--- src/SearchDisplay.jsx
+++ src/SearchDisplay.jsx
@@ -1,16 +1,17 @@
 import SearchForm from "./SearchForm"
 import Courses from "./Courses"
 import { useState } from "react"
+import styles from "./styling/SearchDisplay.module.css"
 
 function SearchDisplay(){
 
   const [courseData, setCourseData] = useState([])    
 
     return(
-        <>
+        <div className={styles.SearchDisplay}>
             <SearchForm setCourseData={setCourseData}/>
             <Courses courseData={courseData}/>
-        </>
+        </div>
     )
 }
 
