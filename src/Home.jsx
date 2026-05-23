a80ad59721a6b0fcb66bc7f838108f7a49753146
Jamie Bryson <jamieab98@gmail.com>
2026-05-23 07:57:01 -0400

    Added styling to Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,13 @@
+import styles from "./styling/Home.module.css"
+
 function Home(){
 
     return(
-        <>
-            <h2>Welcome to U-Discovery</h2>
-            <div>This is an application that lets you find discgolf courses near you or wherever you please! All you need to know is the abbreviation to the country you're searching through and the region!</div>
-            <div>Click the find course tab at the top to start looking for courses!</div>
-        </>
+        <div className={styles.Home}>
+            <h2 className={styles.Heading}>Welcome to U-Discovery</h2>
+            <div className={styles.Description}>This is an application that lets you find discgolf courses near you or wherever you please! All you need to know is the abbreviation to the country you're searching through and the region!</div>
+            <div className={styles.Direction}>Click the find course tab at the top to start looking for courses!</div>
+        </div>
     )
 }
 
